ea8a8bbfe68f0d810a5759733d68157c51e32f3d
ofirx2 <ofirkubla@hotmail.com>
2016-05-25 02:49:54 +0300

    we can write things... simple-Bisection-Pruning algorithm implemented no tests have been made yet correction to two variables with the name L in the paper

--- simpleBP.py
+++ simpleBP.py
@@ -4,9 +4,39 @@
 ###
 
 import numpy as np
+from siminit import link,node,djikstra,cost
 
 ###
 # Instructions:
 # Implement Simple-Bisection Pruning Algorithm
 # (Page 4, top box)
 ###
+
+def simple_bisection_pruning(nodes,links,source,target, constraint, search_count):
+    temporaryE = links
+    saved_route = None
+    route = djikstra(nodes,links,source,target)
+    if route != None and len(route) <= constraint:
+        search = search_count
+        saved_route = route
+        path_concave_cost = cost(route)
+        L = 0
+        U = 1
+    else:
+        search = 0
+    while search != 0:
+        epsilon = (L+U) / 2
+        for link in links:
+            if link.get_cost() >= epsilon * path_concave_cost:
+                links.remove(link)
+        route = djikstra(nodes,links,source,target)
+        if route != None and len(route) <= constraint:
+            saved_route = route
+            path_concave_cost = cost(route)
+            U = (L+U) / 2
+        else:
+            L = (L+U) / 2
+        links = temporaryE
+        search -= 1
+    return saved_route
+        
