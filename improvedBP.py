a93331cda752ab94a9ce82aea9fd3e11a7124509
J <J@J.J>
2016-05-25 13:48:24 +0300

    Added non-woroking code for perusal

--- improvedBP.py
+++ improvedBP.py
@@ -18,18 +18,30 @@ L = 75.0
 #
 #
 
-def improved_bisection_pruning(nodes,links,source,target):
+def improved_bisection_pruning(nnodes,nlinks,source,target):
     newlen = 'R'
     oldlen = 'R'
     search = 1
-    TemporaryE = 'E'
+    TemporaryE = source
     SavedRoute = None
+    nodes = nnodes.clone()
+    links = nlinks.clone()
     while search!=0:
         route = djikstra(nodes, links, source, target)
         if route is not None and len(route)<=L:
             oldlen=newlen
             SavedRoute = route
             PathConcaveCost = cost(route)
-            
-            
+            for link in <node>.links(): #What do I even put here
+                if cost(route)>=epsilon*PathConcaveCost :
+                    route.remove(link)
+                    newlen=PruningFactor*PathConcaveCost;
+        else :
+            for link in <node>.links():
+                if li.cost>=(newlen+oldlen)/2:
+                    route.remove(link)
+            newlen=(newlen+oldlen)/2
+            NumberOfFail -= 1
+            if NumberOfFail == 0 :
+                search = 0            
     return SavedRoute
