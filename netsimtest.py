66c55cbfb1da5dfb40e7a1475f5a5c00a1b4524f
ofirx2 <ofirkubla@hotmail.com>
2016-05-26 04:25:49 +0300

    second mistake in my life

--- netsimtest.py
+++ netsimtest.py
@@ -40,7 +40,7 @@ def simulation_process(search_count, number_of_iteration):
     for i in range(5,31):
         dict_improved[i] = {}
         for j in options:
-            dict_simple[i][j] = 0
+            dict_improved[i][j] = 0
     
         
 
@@ -80,16 +80,12 @@ def simulation_process(search_count, number_of_iteration):
         route_simple = simpleBP.simple_bisection_pruning(node , links, node[source], node[target],hops, search_count )
         route_improved = improvedBP.improved_bisection_pruning(node , links , node[source], node[target],hops)
         
-        print(dict_simple)
-        
         if(route_simple != None):
             dict_simple[hops]["total_costs"] += siminit.cost(route_simple)
             dict_simple[hops]["total_hops"] += len(route_simple)
             dict_simple[hops]["success_count"] += 1
         else:
             dict_simple[hops]["fail_count"] += 1
-        
-        print(dict_improved)
         
         if(route_improved != None):
             dict_improved[hops]["total_costs"] += siminit.cost(route_improved)
